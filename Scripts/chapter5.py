b9b945fbb65dafa143fbd7f49ddc7e64f04fa2f2
Wiley Wiggins <weevil@wileywiggins.com>
2021-05-05 20:35:56 -0700

    fixed bad starting table ids in runes

--- Scripts/chapter5.py
+++ Scripts/chapter5.py
@@ -14,29 +14,27 @@ def getAdjective():
 with open("../Codex/_project/_markdown/Runes.md", "w") as myfile:
     myfile.write("---\ntitle: Runes\ntype: bodymatter\n---\n")
     myfile.write("\n::: chapter:chapter-five\n")
-    myfile.write("\n::: section:page classes:\"break-after\"\n")
     myfile.write("# Runes\n\n")
     myfile.write(chapterQuote.generateQuote())
-    myfile.write("\n::: exit:page\n")
 
     runes = runes.Runes()
     runeCounter= 0
-    myfile.write("\n::: section:runetable-1 classes:\"break-after\"\n")
+    myfile.write("\n::: section:runetable-0 classes:\"break-after\"\n")
     myfile.write("| Glyph | Translation |\n")
     myfile.write("|-|-|\n")
     for rune in sorted(runes.runeDict):
         runeCounter += 1
         myfile.write('|<img src=\"../images/glyphs/'+runes.runeDict[rune]+'\" ' + 'alt=\"A glyph made from wedge-shaped marks\"/> |' + rune + ' |\n')
         if runeCounter % 10 == 0:
-            nextSectionNumber = str(runeCounter *.1)
-            lastSectionNumber = str(runeCounter *.1-1)
+            nextSectionNumber = str(int(runeCounter *.1))
+            lastSectionNumber = str(int(runeCounter *.1-1))
             myfile.write("\n::: exit:runetable-"+ lastSectionNumber + "\n\n")
             myfile.write("\n::: section:runetable-"+ nextSectionNumber +" classes:\"break-after\"\n")
             myfile.write("| Glyph | Translation |\n")
             myfile.write("|-|-|\n")
         if runeCounter >= len(runes.runeDict) and runeCounter % 10 != 0:
             print("I think I'm done")
-            myfile.write("\n::: exit:runetable-"+ lastSectionNumber + "\n\n")
+            myfile.write("::: exit:runetable-"+ lastSectionNumber + "\n\n")
 
 
 
